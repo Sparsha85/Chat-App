ff0238ddbd6d4836d713ff2323bacd8409d3576e
Sparsha85 <65542946+Sparsha85@users.noreply.github.com>
2021-08-29 19:46:11 +0530

    Add files via upload
    Replace the files in the src folder of any create-react-app with the files in these folders.

---
+++ App.js
@@ -0,0 +1,19 @@
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+    return(
+        <ChatEngine 
+            height="100vh"
+            projectID="6c6547c9-e783-40ef-8ae7-7b6c0f5563c5"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps}/>}
+        />
+    )
+}
+
+export default App;
