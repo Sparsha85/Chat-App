d9dbcc744c262f7958e682a150c02469c253f26b
Sparsha85 <65542946+Sparsha85@users.noreply.github.com>
2021-08-31 20:04:18 +0530

    Update guide.jsx

--- components/guide.jsx
+++ components/guide.jsx
@@ -3,4 +3,5 @@ One is what controls the main chat window (Chat Feed),
 the other takes in the message (Message Form),
 another displays your messages (My messages),
 another shows the third partie's messages (Their Message),
-and one is the Login Form (LoginForm). */
+and one is the Login Form (LoginForm). 
+*/
